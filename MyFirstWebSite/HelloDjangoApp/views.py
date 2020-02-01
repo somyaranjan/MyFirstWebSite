3e10a0e9986c9d8d17dfa4e4908d1e176ba4e960
Somyaranjan Garnaik <ranjan.intel@gmail.com>
2020-02-01 18:27:12 +0530

    test1

---
+++ MyFirstWebSite/HelloDjangoApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : "on" + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
+def about(request):
+    return render(
+        request,
+        "HelloDjangoApp/about.html",
+        {
+            'title' : "About HelloDjangoApp",
+            'content' : "Example app page for Django."
+        }
+    )
